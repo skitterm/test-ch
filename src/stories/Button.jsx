c46c1e369c7ab37183a1412ffb896339e0cce7dd
Steven Kitterman <steven.kitterman@gmail.com>
2022-04-19 15:27:36 -0700

    Added a word

--- src/stories/Button.jsx
+++ src/stories/Button.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import './button.css';
 
 /**
- * Primary UI component for user interaction
+ * Primary UI component for user interactions
  */
 export const Button = ({ primary, backgroundColor, size, label, ...props }) => {
   const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
